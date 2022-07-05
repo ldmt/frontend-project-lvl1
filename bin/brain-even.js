1c7f493a35f3ee16874d960d77939c026334cdbe
Leonid Matrenin <lndmtrnn@gmail.com>
2022-07-05 03:06:02 +0300

    delete game logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,39 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import welcome from '../src/cli.js';
+import gameEven from '../games/logic-brain-even.js';
 
-const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
-
-const evenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = welcome();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  let correctAnswers = 0;
-  while (correctAnswers !== 3) {
-    const randomInt = Math.floor(Math.random() * 100);
-    console.log(`Question: ${randomInt}`);
-    const answer = readlineSync.question('Your answer: ');
-
-    if (!['yes', 'no'].includes(answer)) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answer === 'yes' ? 'no' : 'yes'}'.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
-    }
-
-    if (answer === isEven(randomInt)) {
-      correctAnswers += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answer === 'yes' ? 'no' : 'yes'}'.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
-    }
-  }
-
-  if (correctAnswers === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
-};
-
-evenGame();
+gameEven();
